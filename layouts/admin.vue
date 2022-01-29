595a64fa774e7b92e3d9c983a828941fd91f1a7d
0u7 <u7@linyuqingde-iMac.local>
2022-01-29 22:26:35 +0800

    default ui components

---
+++ layouts/admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+
+</script>
+
+<style>
+  body {
+    font-family: 'Baloo Bhaijaan 2', cursive;
+  }
+
+  h1 {
+    font-family: 'Supermercado One', cursive;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <the-header @sidenavToggle="displaySidenav = !displaySidenav" />
+    <the-sidenav @close="displaySidenav = false" :show="displaySidenav" />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+  import TheHeader from '@/components/Navigation/TheHeader.vue'
+  import TheSidenav from '@/components/Navigation/TheSidenav.vue'
+  export default {
+    components: {
+      TheHeader,
+      TheSidenav
+    },
+    data() {
+      return {
+        displaySidenav: false
+      }
+    }
+  }
+
+</script>
+
+<style>
+  body {
+    font-family: 'Baloo Bhaijaan 2', cursive;
+  }
+
+  h1 {
+    font-family: 'Supermercado One', cursive;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
