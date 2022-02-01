cc2f52c5be364830035c4df3cbc88cb6d3ae1a1a
0u7 <u7@linyuqingde-iMac.local>
2022-02-01 18:50:04 +0800

    post and patch post by vuex store function

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="edit-post-page">
     <section>
-      <edit-post-form :existingPost="post">
+      <edit-post-form :existingPost="post" @save="onUpdatePost">
         <h1>Edit post Id: {{ post.postId }}</h1>
       </edit-post-form>
     </section>
@@ -10,20 +10,34 @@
 
 
 <script>
+  import axios from 'axios'
   import EditPostForm from '@/components/Admin/EditPostForm.vue'
   export default {
     layout: 'admin',
     components: {
       EditPostForm
     },
-    data() {
-      return {
-        post: {
-          postId: '123456789',
-          author: 'Alex',
-          title: 'The existing post title.',
-          content: 'The existing post content here.........'
-        }
+    asyncData(content) {
+      const postId = content.params.postId
+      return axios.get(
+          `https://nuxt-practice-1cc8e-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${postId}.json`)
+        .then(result => {
+          return {
+            post: {
+              ...result.data,
+              postId: postId
+            }
+          }
+        }).catch(e => {
+          console.log(e)
+        })
+    },
+    methods: {
+      onUpdatePost(updatePost) {
+        this.$store.dispatch('editPost', updatePost).then(()=>{
+          this.$router.push('/admin')
+        })
+        
       }
     }
   }
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <edit-post-form>
+    <edit-post-form @save="onSavePost">
       <h1>Add new post!</h1>
     </edit-post-form>
   </section>
@@ -8,12 +8,20 @@
 
 
 <script>
+import axios from 'axios'
   import EditPostForm from '@/components/Admin/EditPostForm.vue'
   export default {
     layout: 'admin',
     components: {
       EditPostForm,
     },
+    methods: {
+      onSavePost(newPost) {
+        this.$store.dispatch('addPost', newPost).then(()=>{
+          this.$router.push('/admin')
+        })   
+      }
+    }
   }
 
 </script>
