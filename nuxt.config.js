f2fad7464150125aefe91269761885e1f3a013fe
0u7 <u7@linyuqingde-iMac.local>
2022-02-01 18:45:56 +0800

    add axios for fetching data from firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,8 +51,11 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
   ],
-
+  axios: {
+    // proxy: true
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
 }
